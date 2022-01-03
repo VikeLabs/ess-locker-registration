3880e80abb211ab52f8efc5484f2005d13c3f2fb
edorableraf <55640599+edorableraf@users.noreply.github.com>
2022-01-02 20:00:22 -0800

    Add files via upload

---
+++ server/controllers/admin.js
@@ -0,0 +1,35 @@
+import dbo from "../db/conn.js";
+
+module.exports = {
+    resolve = async (req, res) => {
+        const dbConnect = dbo.getDb();
+        const filter = {
+            building: req.body.building,
+            number: parseInt(req.body.number),
+            status: 'registered',
+            reported: true
+        };
+        
+        const updateDoc = {
+            $set: {
+                reported: false,
+                updatedAt: new Date()
+            }
+        };
+    
+        dbConnect
+            .collection('lockers')
+            .updateOne(filter, updateDoc)
+            .then(result => {
+                if (result.matchedCount === 1) {
+                    res.json({msg: "success"});
+                } else {
+                    res.json({err: "not found"});
+                }
+            })
+            .catch(err => {
+                res.status(400).send('Error resolving locker');
+                console.log(err);
+            });
+    }
+};
