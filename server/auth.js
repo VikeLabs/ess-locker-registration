2ac818ec3c9ecf4576fb675217ec99463ce431e4
Rafael Edora <rvedora@gmail.com>
2022-05-16 19:30:54 -0700

    auth update

---
+++ server/auth.js
@@ -0,0 +1,70 @@
+// auth.js
+
+/**
+ * Required External Modules
+ */
+
+import { Router } from 'express';
+const router = Router();
+import passport from 'passport';
+
+/**
+ * Routes Definitions
+ */
+
+router.get("/login",
+    passport.authenticate("auth0", {
+        scope: "openid email profile"
+    }),
+    (req, res) => res.redirect("/")
+);
+
+router.get("/callback", (req, res, next) => {
+    passport.authenticate("auth0", (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.redirect("/login");
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            const returnTo = req.session.returnTo;
+            delete req.session.returnTo;
+            res.redirect(returnTo || "/");
+        });
+    })(req, res, next);
+});
+
+router.get("/logout", (req, res) => {
+    console.log('logout');
+    req.logOut();
+
+    let returnTo = req.protocol + "://" + req.hostname;
+    const port = req.socket.localPort;
+
+    if (port !== undefined && port !== 80 && port !== 443) {
+        returnTo =
+            process.env.NODE_ENV === "production"
+            ? `${returnTo}/`
+            : `${returnTo}:${port}/`;
+    }
+   
+    const logoutURL = new URL(
+        `https://${process.env.AUTH0_DOMAIN}/v2/logout`
+    );
+   
+    const params = new URLSearchParams({
+        client_id: process.env.AUTH0_CLIENT_ID,
+        returnTo: returnTo
+    })
+    const searchString = params.toString();
+
+    logoutURL.search = searchString;
+
+    res.redirect(logoutURL);
+});
+
+export default router;
