1fa55bc3b32089ea114b0ef0a16d0bec2eb60e3d
hungry-yumyumman <71944561+hungry-yumyumman@users.noreply.github.com>
2022-07-07 18:30:26 -0700

    Fixed report list

--- routesTester/src/script.js
+++ routesTester/src/script.js
@@ -4,18 +4,24 @@ const registerResult = document.getElementById('register-result');
 const deregisterResult = document.getElementById('deregister-result');
 const resolveResult = document.getElementById('resolve-result');
 const countResult = document.getElementById('count-result');
+const resolve_result = document.getElementById('a-resolve-result');
+const admin_deregister_result = document.getElementById('a-deregister-result');
+const admin_register_result = document.getElementById('a-register-result')
+const admin_deregister_all_result = document.getElementById('deregister-all-result');
+const statusResult = document.getElementById('status-result');
+const reportlistResult = document.getElementById('reportlist-result');
+
 
 async function put(uri = '', data = {}) {
     const response = await fetch(uri, {
         method: 'PUT',
+        mode: 'cors',
         headers: {
-          'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data) 
+        redirect: 'follow',
+        body: JSON.stringify(data)  
     });
-    if (response.status === 403) {
-        return {err: 'forbidden'};
-    }
     return response.json();
 }
 
@@ -26,7 +32,7 @@ async function get(uri = '') {
 
 async function report(building, number) {
     const data = {"building": building, "number": number};
-    put(`http://localhost:8000/report/`, data)
+    put(`http://localhost:5000/report/`, data)
     .then(result => {
         reportResult.innerHTML = (result.msg) ? result.msg : result.err;
     });
@@ -34,14 +40,14 @@ async function report(building, number) {
 
 async function resolve(building, number) {
     const data = {"building": building, "number": number};
-    put(`http://localhost:8000/resolve/`, data)
+    put(`http://localhost:5000/resolve/`, data)
     .then(result => {
         resolveResult.innerHTML = (result.msg) ? result.msg : result.err;
     });
 }
 
 async function search(building, number) {
-    get(`http://localhost:8000/search/building/${building}/number/${number}`)
+    get(`http://localhost:5000/search/building/${building}/number/${number}`)
     .then(locker => {
         console.log(locker);
         searchResult.innerHTML = `Building: ${locker.building}, number: ${locker.number}`;
@@ -52,7 +58,7 @@ async function search(building, number) {
 
 async function register(building, number, user, userEmail) {
     const data = {'building': building, 'number': number, 'user': user, 'userEmail': userEmail};
-    put(`http://localhost:8000/register/`, data)
+    put(`http://localhost:5000/register/`, data)
     .then(result => {
         registerResult.innerHTML = (result.msg) ? result.msg : result.err;
     });
@@ -60,15 +66,63 @@ async function register(building, number, user, userEmail) {
 
 async function deregister(building, number, user, userEmail) {
     const data = {'building': building, 'number': number, 'user': user, 'userEmail': userEmail};
-    put(`http://localhost:8000/deregister/`, data)
+    put(`http://localhost:5000/deregister/`, data)
     .then(result => {
         deregisterResult.innerHTML = (result.msg) ? result.msg : result.err;
     });
 }
 
 async function count() {
-    get(`http://localhost:8000/count/`)
+    get(`http://localhost:5000/count/`)
     .then(result => {
         countResult.innerHTML = `total: ${result.totalCount}, available: ${result.availableCount}, registered: ${result.registeredCount}`;
     });
+}
+
+async function admin_deregister(building, number, user, userEmail){
+    const data = {"building": building,"number": number, 'user': user, 'userEmail': userEmail };
+    put(`http://localhost:5000/admin_deregister/`, data)
+    .then(result => {
+        admin_deregister_result.innerHTML = (result.msg) ? result.msg : result.err;
+    })
+}
+
+async function admin_register(building, number, user, userEmail){
+    const data = {"building": building,"number": number, 'user': user, 'userEmail': userEmail};
+    put(`http://localhost:5000/admin_register/`, data)
+    .then(result => {
+        admin_register_result.innerHTML = (result.msg) ? result.msg : result.err;
+    })
+} 
+
+async function deregister_all(building, number){
+    const data = {};
+    put(`http://localhost:5000/admin_deregister_all/`)
+    .then(result => {
+        admin_deregister_all_result.innerHTML = (result.msg) ? result.msg : result.err;
+    })
+}
+
+async function status(building, number) {
+    get(`http://localhost:5000/status/building/${building}/number/${number}`)
+    .then(locker => {
+        console.log(locker);
+        statusResult.innerHTML = `${locker.building} ${locker.number} is ${locker.status}`;
+    }).catch(error =>{
+        console.log(error)
+    })
+}
+
+async function report_list() {
+    get(`http://localhost:5000/report_list/`)
+    .then(lockers => {
+        console.log(lockers);
+
+        lockers.forEach(locker => {
+            reportlistResult.innerHTML += `${locker.building} ${locker.number} has been reported <br>`;
+        });
+        
+    }).catch(error =>{
+        console.log(error)
+    })
 }
