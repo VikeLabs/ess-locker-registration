d35962d85c157ccf6012f517818145c133d04fde
Aomi <aomi@users.noreply.github.com>
2022-10-11 13:24:53 -0700

    chore: Next.js migration (#3)
    * chore: initial commit with next.js installed with tailwind
    
    * feat: dynamic page routing example
    
    * feat: mongo client example
    
    * fix: connection caching
    
    * feat: db schema
    
    * feat: setup testing
    
    * feat: db client
    
    * feat: api examples
    
    * feat: remove mongo wooo~
    
    * feat: integration tests with sqlite
    
    * chore: sqlite3
    
    * fix: path

---
+++ pages/contact.tsx
@@ -0,0 +1,15 @@
+export default function Contact() {
+  return (
+    <div>
+      <div>
+        <h1>Contact</h1> <br />
+        <p>
+          Lockers system maintainer: <a href="mailto:">Name</a>
+        </p>
+        <p>
+          ESS Director of IT: <a href="mailto:">Name</a>
+        </p>
+      </div>
+    </div>
+  );
+}
