b94d7b3368e3fa9cca5950c1d8f84b5e9236594f
hungry-yumyumman <71944561+hungry-yumyumman@users.noreply.github.com>
2023-01-03 19:10:02 -0800

    Made updates to user.ts

--- lib/api/users/user.ts
+++ lib/api/users/user.ts
@@ -1,27 +1,22 @@
 import { db} from "../../db";
 
 export function getlockers() {
-    return db.prepare("SELECT * FROM lockers").get()
+    return db.prepare("SELECT * FROM lockers").all(); // Returns array of all locker row objects in the 'locker' table
 }
 
-export function register(building: number, number: number, name: string) {
+export function register(building: number, number: number, name: string, email: string) {
 
-    const user_statement = db.prepare("INSERT INTO users VALUES(?)").run(name)
+    const user_statement = db.prepare("INSERT INTO users (name, email) VALUES(?, ?)").run(name, email)
     const statement = db.prepare("INSERT INTO registrations (building_id, num, reported_at, user_id) VALUES (?, ?, ?, ?)")
 
     const info = statement.run(building, number, Date.now(), user_statement.lastInsertRowid)
+    return info.lastInsertRowid
 }
 
 
 export function deregister(building: number, number: number) {
-    const statement = db.prepare("DELETE FROM registrations WHERE building_id=? AND num=?")
+    const statement = db.prepare("DELETE FROM registrations WHERE building_id = ? AND num = ?")
 
     const info = statement.run(building, number)
+    return info.changes
 }
-
-// export function report(building: number, number: number){
-    
-// }
-
-
-
