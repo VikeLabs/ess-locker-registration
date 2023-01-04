a2c3b392727095ad6ae861bcc5f43e88214673b6
hungry-yumyumman <71944561+hungry-yumyumman@users.noreply.github.com>
2023-01-03 19:07:17 -0800

    Made updates to admin.test.ts

--- lib/api/admin/admin.test.ts
+++ lib/api/admin/admin.test.ts
@@ -1,10 +1,36 @@
-import {getlockers, deregisterall, getregistrations, getusers} from "./admin";
+import { initDB } from "../../db/init";
+import {getlockers, getusers, getregistrations, deregisterall} from "./admin";
 
-describe("building service", () => {
+describe("Testing Admin Functions", () => {
+    it("returns true", () => {
+        expect(initDB()).toBeTruthy();
+    });
+
     describe("Testing getlockers", () => {
-        it("creates a building", () => {
+        it("Returns all lockers in ELW and ECS", () => {
             const lockers_list = getlockers();
             expect(lockers_list).toHaveLength(838);
+          });
+    })
+
+    describe("Testing getusers", () => {
+        it("Returns all the name and emails of users in the database", () => {
+            const user_list = getusers();
+            expect(user_list).toHaveLength(0);
+          });
+    })
+
+    describe("Testing getregistrations", () => {
+        it("Returns all the registrations in the database", () => {
+            const registration_list = getregistrations();
+            expect(registration_list).toHaveLength(0);
+          });
+    })
+
+    describe("Testing deregisterall", () => {
+        it("Returns an array of all the deregistered lockers", () => {
+            const deregistered_list = deregisterall();
+            expect(deregisterall).toHaveLength(0);
           });
     })
 
