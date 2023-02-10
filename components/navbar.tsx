2cfe604c96754f288a89a50a75920ae09ffd506a
Rafael Edora <rvedora@gmail.com>
2023-02-09 21:02:38 -0800

    cleaned up nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,25 +1,17 @@
 export default function Navbar() {
     return (
         <div className="flex items-center justify-start bg-gray-800 p-3 space-x-3">
-            <a href="/">
-                <div className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-1 px-4 rounded">
-                    Hello!
-                </div>
+            <a href="/" className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-1 px-4 rounded">
+                Hello!
             </a>
-            <a href="/deregister">
-                <div className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-1 px-4 rounded">
-                    I
-                </div>
+            <a href="/deregister" className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-1 px-4 rounded">
+                I
             </a>
-            <a href="/">
-                <div className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-1 px-4 rounded">
-                    am
-                </div>
+            <a href="/" className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-1 px-4 rounded">
+                am
             </a>
-            <a href="/">
-                <div className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-1 px-4 rounded">
-                    Groot!
-                </div>
+            <a href="/" className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-1 px-4 rounded">
+                Groot!
             </a>
         </div>
     )
