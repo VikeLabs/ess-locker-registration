b40d2ab9f51f5727493bafb9843b5725288381b4
hungry-yumyumman <71944561+hungry-yumyumman@users.noreply.github.com>
2023-03-01 20:11:58 -0800

    Changed return values

--- lib/api/admin/admin.ts
+++ lib/api/admin/admin.ts
@@ -17,3 +17,35 @@ export function deregisterAll(){
     return stmt.changes
 }
 
+export function register(building: number, number: number, name: string, email: string):boolean {
+
+    try{
+
+        const user_statement = db.prepare("INSERT INTO users (name, email) VALUES(?, ?)").run(name, email)
+        const statement = db.prepare("INSERT INTO registrations (building_id, num, reported_at, user_id) VALUES (?, ?, ?, ?)")
+
+        const info = statement.run(building, number, null, user_statement.lastInsertRowid)
+        return (info.changes > 0)
+    }
+    catch(error){
+
+        return(false)
+
+    }
+}
+
+export function deregister(building: number, number: number, name: string, email: string):boolean { // Add comments explaining why function would be false
+
+    try{
+        const user = db.prepare("SELECT id FROM users WHERE name = ? AND email = ?").get(name, email)
+        
+        const statement = db.prepare("DELETE FROM registrations WHERE building_id = ? AND num = ? AND user_id = ?")
+        const info = statement.run(building, number, user.id)
+
+        return (info.changes > 0)
+    }
+    catch(error){
+        return (false)
+    }
+    
+}
