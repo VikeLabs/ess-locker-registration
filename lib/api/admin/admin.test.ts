ad67bd17837db99837a378b6cf8e1b056e9bb511
hungry-yumyumman <71944561+hungry-yumyumman@users.noreply.github.com>
2023-03-01 20:13:56 -0800

    Changed test descriptions

--- lib/api/admin/admin.test.ts
+++ lib/api/admin/admin.test.ts
@@ -1,7 +1,7 @@
 import { initDB } from "../../db/init";
-import {getLockers, getUsers, getRegistrations, deregisterAll} from "./admin";
+import {getLockers, getUsers, getRegistrations, deregisterAll, register, deregister} from "./admin";
 
-describe("Testing Admin Functions", () => {
+describe("Testing Admin Controller in different scenarios after filling database with lockers", () => { //Will rename this later
     it("returns true", () => {
         expect(initDB()).toBeTruthy();
     });
@@ -20,17 +20,31 @@ describe("Testing Admin Functions", () => {
           });
     })
 
+    describe("Testing register with an existing locker and a non-existent user", () => {
+        it("Should register a new locker using the provided name, email and locker", () => {
+            const registrations = register(1, 34, "Chicken Run", "Take@gmail.com");
+            expect(registrations).toBe(true);
+          });
+    })
+
+    describe("Testing register with a registered existing locker and a non-existent user", () => {
+        it("Should not register an already registered locker using the provided name, email and locker", () => {
+            const registrations = register(1, 34, "Darla Dupe", "Dish@gmail.com");
+            expect(registrations).toBe(false);
+          });
+    })
+
     describe("Testing getregistrations", () => {
         it("Returns all the registrations in the database", () => {
             const registration_list = getRegistrations();
-            expect(registration_list).toHaveLength(0);
+            expect(registration_list).toHaveLength(1);
           });
     })
 
     describe("Testing deregisterall", () => {
         it("Returns number of deregistered lockers", () => {
             const deregistered_list = deregisterAll();
-            expect(deregistered_list).toBe(0);
+            expect(deregistered_list).toBe(1);
           });
     })
 
