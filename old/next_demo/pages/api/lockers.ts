229b664a444066da6d2b1a83acb99d79ccbafacb
Rafael Edora <rvedora@gmail.com>
2023-06-28 19:36:57 -0700

    removed demo files

---
+++ old/next_demo/pages/api/lockers.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../../lib/db";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    const lockers = db.prepare("SELECT * FROM lockers").all();
+    res.status(200).json(lockers);
+    return;
+  } else if (req.method === "POST") {
+    const { name } = req.body;
+    const stmt = db.prepare(
+      `INSERT INTO lockers (name,created_at,updated_at) VALUES (?,?,?)`
+    );
+    const now = new Date().toISOString();
+    const info = stmt.run(name, now, now);
+    res.status(200).json(info);
+    return;
+  }
+}
