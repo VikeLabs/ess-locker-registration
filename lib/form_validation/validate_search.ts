41046c30bc314d7e8750a90a08f5a0f1916a9d83
Rafael Edora <rvedora@gmail.com>
2023-09-14 21:14:42 -0700

    moved alert out of validate logic to improve tests

--- lib/form_validation/validate_search.ts
+++ lib/form_validation/validate_search.ts
@@ -3,14 +3,12 @@ import { ELW_ID, ELW_COUNT, ECS_ID, ECS_COUNT } from '../locker_constants';
 export default function validateSearch(building: number, number: number): boolean | void {
     if (building === ELW_ID) {
         if (number < 1 || number > ELW_COUNT) {
-            alert('Invalid locker number');
             return false;
         }
     }
 
     if (building === ECS_ID) {
         if (number < 1 || number > ECS_COUNT) {
-            alert('Invalid locker number');
             return false;
         }
     }
