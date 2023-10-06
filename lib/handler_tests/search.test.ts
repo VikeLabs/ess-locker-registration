2bb08a3a7cd3294a516bc405cf4f312eee233c39
Rafael Edora <rvedora@gmail.com>
2023-10-05 21:32:38 -0700

    update tests

--- lib/handler_tests/search.test.ts
+++ lib/handler_tests/search.test.ts
@@ -1,6 +1,6 @@
 import { initDB } from "../db/init";
 import { ECS_ID, ELW_ID } from "../locker_constants";
-import { register } from "../api/users/user";
+import { register, report } from "../api/users/user";
 import handler from "../../pages/api/search";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -8,53 +8,69 @@ const mockedJson = jest.fn();
 
 const mockedRes = {
     status: jest.fn().mockReturnValue({ json: mockedJson }),
+    redirect: jest.fn(),
 } as unknown as jest.Mocked<NextApiResponse>;
 
-initDB();
 
-describe("Testing search handler", () => {
+describe("Search handler", () => {
 
-    it("Returns HTTP 200 when the locker exists", async () => {
+    beforeEach(() => {
+        initDB();
+        jest.clearAllMocks();
+    });
+
+    it("Redirects to register when the locker is available", async () => {
+        const lockerNum = 101;
         const req = {
             method: "GET",
             query: {
                 building: ECS_ID,
-                number: 101
+                number: lockerNum
             },
         } as unknown as NextApiRequest;
 
         handler(req, mockedRes);
 
-        expect(mockedRes.status).toHaveBeenLastCalledWith(200);
+        expect(mockedRes.status).not.toHaveBeenCalled();
+        expect(mockedRes.redirect).toHaveBeenCalledTimes(1);
+        expect(mockedRes.redirect).toHaveBeenCalledWith(200, `/register?building=${ECS_ID}&number=${lockerNum}`);
     });
 
-    it("Returns registration information when the locker is registered", async () => {
-        register(ECS_ID, 101, "John Doe", "johndoe@uvic.ca", false);
+    it("Redirects to deregister when the locker is not available", async () => {
+        const lockerNum = 101;
+        register(ECS_ID, lockerNum, "John Doe", "johndoe@uvic.ca", false);
         const req = {
             method: "GET",
             query: {
                 building: ECS_ID,
-                number: 101
+                number: lockerNum
             },
         } as unknown as NextApiRequest;
 
         handler(req, mockedRes);
 
-        expect(mockedJson).toHaveBeenLastCalledWith({ available: false, building_id: ECS_ID, num: 101, reported_at: null });
+        expect(mockedRes.status).not.toHaveBeenCalled();
+        expect(mockedRes.redirect).toHaveBeenCalledTimes(1);
+        expect(mockedRes.redirect).toHaveBeenCalledWith(200, `/deregister?building=${ECS_ID}&number=${lockerNum}`);
     });
 
-    it("Returns registration information when the locker is not registered", async () => {
+    it("Redirects to deregister when the locker is not available and reported", async () => {
+        const lockerNum = 101;
+        register(ECS_ID, lockerNum, "John Doe", "johndoe@uvic.ca", false);
+        report(ECS_ID, lockerNum);
         const req = {
             method: "GET",
             query: {
-                building: ELW_ID,
-                number: 201
+                building: ECS_ID,
+                number: lockerNum
             },
         } as unknown as NextApiRequest;
 
         handler(req, mockedRes);
 
-        expect(mockedJson).toHaveBeenLastCalledWith({ available: true, building_id: ELW_ID, num: 201, reported_at: null });
+        expect(mockedRes.status).not.toHaveBeenCalled();
+        expect(mockedRes.redirect).toHaveBeenCalledTimes(1);
+        expect(mockedRes.redirect).toHaveBeenCalledWith(200, `/deregister?building=${ECS_ID}&number=${lockerNum}&reported=true`);
     });
 
     it("Returns HTTP 404 when the locker does not exist", async () => {
@@ -68,7 +84,11 @@ describe("Testing search handler", () => {
 
         handler(req, mockedRes);
 
-        expect(mockedRes.status).toHaveBeenLastCalledWith(404);
+        expect(mockedRes.redirect).not.toHaveBeenCalled();
+        expect(mockedRes.status).toHaveBeenCalledTimes(1);
+        expect(mockedRes.status).toHaveBeenCalledWith(404);
+        expect(mockedJson).toHaveBeenCalledTimes(1);
+        expect(mockedJson).toHaveBeenCalledWith({ message: "Locker not found" });
     });
 
     it("Returns HTTP 400 when the query is invalid", async () => {
@@ -82,6 +102,10 @@ describe("Testing search handler", () => {
 
         handler(req, mockedRes);
 
+        expect(mockedRes.redirect).not.toHaveBeenCalled();
+        expect(mockedRes.status).toHaveBeenCalledTimes(1);
         expect(mockedRes.status).toHaveBeenLastCalledWith(400);
+        expect(mockedJson).toHaveBeenCalledTimes(1);
+        expect(mockedJson).toHaveBeenCalledWith({ message: "Invalid query" });
     });
 });
